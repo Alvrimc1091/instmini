f37a9d6ab80c18a0a66d2b59f60b92c64e1e829c
Alvrimc1091 <89285922+Alvrimc1091@users.noreply.github.com>
2023-07-24 21:07:56 -0400

    Create send_command.py

---
+++ lab_tests/send_command.py
@@ -0,0 +1,36 @@
+import serial
+
+# Configuración de la comunicación serial
+port = '/dev/ttyUSB0'  # Puerto USB al que está conectado el dispositivo
+baudrate = 9600  # Velocidad de transmisión en baudios
+
+# Crear objeto Serial
+ser = serial.Serial(port, baudrate)
+
+# Abrir el puerto serial
+ser.open()
+
+# Verificar si el puerto está abierto
+if ser.is_open:
+    print('Conexión establecida. Puedes enviar comandos.')
+
+    # Ciclo principal
+    while True:
+        # Leer el comando desde la terminal de la Raspberry Pi
+        comando = input('Ingrese el comando ASCII: ')
+
+        # Enviar el comando al dispositivo
+        ser.write(comando.encode())
+
+        # Leer la respuesta del dispositivo, si es necesario
+        # respuesta = ser.readline().decode()
+        # print('Respuesta del dispositivo:', respuesta)
+
+        # Comprobar si se debe terminar el programa
+        if comando.lower() == 'exit':
+            break
+
+# Cerrar el puerto serial
+ser.close()
+
+print('Programa finalizado.')
