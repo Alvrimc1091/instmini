3b2cf17762966c6cdcd6965049a836981927cd5b
Alvrimc1091 <aimc2918@hotmail.com>
2023-08-04 13:13:52 -0400

    funcaa

--- lab_tests/synth/test7.py
+++ lab_tests/synth/test7.py
@@ -25,36 +25,61 @@ hidapi.hid_set_nonblocking(device, 1)
 print('Conexión establecida. Puedes enviar comandos.')
 
 def send_command(device, command):
-    # Convert the command to bytes and send it to the device
-    result = hidapi.hid_write(device, command.encode(), len(command.encode()))
-    if result == -1:
-        print('Error al enviar el comando.')
-        return False
-    else:
-        print('Comando enviado correctamente.')
-        return True
+    # Debug: Print the parameters
+    print(f'Device handle: {device}')
+    print(f'Command: {command}')
+    command_bytes = command.encode()
+    print(f'Command bytes: {command_bytes}')
+    command_length = len(command_bytes)
+    print(f'Command length: {command_length}')
 
-def get_response(device, timeout=1.0):
+    # Attempt to send the command until successful
+    while True:
+        result = hidapi.hid_write(device, command_bytes, command_length)
+        if result == -1:
+            print('Error al enviar el comando. Reintentando...')
+        else:
+            print(f'Comando enviado correctamente: {command}')
+            return True
+
+        # Add a delay before reattempting
+        time.sleep(0.5)
+
+def get_response(device, command, timeout=1.0):
     # Read response from the device (adjust the buffer size as needed)
     buffer_size = 64
     response = ctypes.create_string_buffer(buffer_size)
     start_time = time.time()
-    bytes_received = 0
+
+    # Debug: Print the parameters
+    print(f'Device handle: {device}')
+    print(f'Command: {command}')
+    command_bytes = command.encode()
+    print(f'Command bytes: {command_bytes}')
+    command_length = len(command_bytes)
+    print(f'Command length: {command_length}')
 
     while time.time() - start_time < timeout:
-        bytes_read = hidapi.hid_read(device, response + bytes_received, buffer_size - bytes_received)
+        bytes_read = hidapi.hid_read(device, response, buffer_size)
         if bytes_read > 0:
-            bytes_received += bytes_read
-        elif bytes_received > 0:
-            break  # Exit the loop if no new data received and we have some data
+            # Decode received bytes back to a string using ASCII encoding
+            response_data = response.raw[:bytes_read].decode("ascii")
+            print(f'Response data: {response_data}')  # Debug: Print the response data
+            if command == 'T':
+                # Handle temperature response (format: T, then read data)
+                temperature_data = response_data.split(',')[1]
+                return temperature_data
+            elif command == '?':
+                # Handle status response (format: 8 bits indicating the state of the device)
+                return response_data
+            else:
+                # Handle other commands with 16-byte data response (if available)
+                return response_data
 
         time.sleep(0.1)
 
-    if bytes_received > 0:
-        # Decode received bytes back to a string using ASCII encoding
-        return response.raw[:bytes_received].decode("ascii")
-    else:
-        return None
+    print('No se recibió ninguna respuesta del dispositivo.')
+    return None
 
 def get_status():
     while True:
@@ -63,7 +88,7 @@ def get_status():
             break
         
         if send_command(device, command):
-            response = get_response(device)
+            response = get_response(device, command)
             if response is not None:
                 print(f'Respuesta del dispositivo: {response}')
             else:
